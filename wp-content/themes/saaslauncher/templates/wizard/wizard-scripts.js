ed462bc66480b6f2c9a1ddf7e9c9cb88168dfbc4
Mike <mike@mikerobinson.co.nz>
2025-11-28 17:54:01 +1300

    Fix submission_ref - use submission_id from API response

--- wp-content/themes/saaslauncher/templates/wizard/wizard-scripts.js
+++ wp-content/themes/saaslauncher/templates/wizard/wizard-scripts.js
@@ -287,7 +287,7 @@ document.addEventListener("DOMContentLoaded", function () {
           const wizardData = {
             goal: goal,
             workflow: workflow,
-            submission_ref: data.submission_ref || sessionId
+            submission_ref: data.submission_id || data.submission_ref || sessionId
           };
           localStorage.setItem('mgrnz_wizard_data', JSON.stringify(wizardData));
 
